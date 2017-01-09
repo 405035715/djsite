7cbadea12eb722148812caea478d289d50ac23a9
zyh <405035715@qq.com>
2017-01-09 21:53:06 +0800

    a

--- jdmonitor/urls.py
+++ jdmonitor/urls.py
@@ -5,5 +5,4 @@ app_name = 'jdmonitor'
 
 urlpatterns = [
     url(r'hospitals/$', views.hospitals_from_jdimage, name='hospitals_from_jdimage'),
-    # url(r'update_monitor_data', views.update_monitor_data, name='update_monitor_data')
 ]
